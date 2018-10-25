eec233eecaa3004fe75dffa9efc9938655f29a3f
Anna Quaglieri <anna.quaglieri16@gmail.com>
2018-10-25 18:57:46 +1100

    fixed parse_gatk_coverage.R

--- R/parse_gatk_coverage.R
+++ R/parse_gatk_coverage.R
@@ -3,8 +3,8 @@
 parse_gatk_coverage <- function(path_to_gatk_coverage,
                                 truth_set,TCGA=FALSE){
 
-# truthset should contain  
-  
+# truthset should contain
+
 cov <- read.delim(path_to_gatk_coverage)
 cov$Locus <- as.character(cov$Locus)
 
@@ -40,7 +40,7 @@ for(i in 1:nrow(create_match_names_alt)){
   create_match_names_alt$SampleName[i] <- mat
 }
 
-# Match Samplename to alt and tot depth 
+# Match Samplename to alt and tot depth
 cov_long_alt_depth1 <- cov_long_alt_depth %>% dplyr::left_join(create_match_names_alt) %>% select(- base_alt_counts)
 cov_long_tot_depth1 <- cov_long_tot_depth %>% dplyr::left_join(create_match_names_tot) %>% select(- base_tot_counts)
 
@@ -59,12 +59,16 @@ cov_long_infos1 <- cov_long_infos %>% separate(alleles,into = c("A","C","G","T",
 #cov_long_infos1[,c("SampleNameShort","Locus","A","C")]
 
 cov_long_infos1 <- cov_long_infos1 %>% dplyr::rename(tot_depth_GATK = tot_depth)
+cov_long_infos1$tot_depth <- as.character(as.numeric(cov_long_infos1$tot_depth))
+
 cov_long_infos1$alt_depth_GATK <- NA
 for(i in 1:nrow(cov_long_infos1)){
   allele_alt <- cov_long_infos1$alt_initial[i]
   allele_alt_dep <- cov_long_infos1[i,allele_alt]
-  cov_long_infos1$alt_depth_GATK[i] <- as.numeric(as.character(gsub(paste0(allele,":"),"",allele_alt_dep)))
+  cov_long_infos1$alt_depth_GATK[i] <- as.numeric(as.character(gsub(paste0(allele_alt,":"),"",allele_alt_dep)))
 }
+
+cov_long_infos1$VAF_GATK <- cov_long_infos1$alt_depth_GATK/cov_long_infos1$tot_depth_GATK
 
 return(cov_long_infos1)
 
@@ -84,15 +88,15 @@ return(cov_long_infos1)
 #                                          Feature=ensembl_transcript_id) %>%
 #   select(SampleName,SampleNameShort,Locus,chrom,pos,end,ref,alt_initial,NCBI,SYMBOL,variant_type,Feature,PresentInRNA,
 #          NormalRefReads,NormalVarReads,NormalVAF,TumorRefReads,TumorVarReads,TumorVAF,RNARefReads,RNAVarReads,RNAVAF,RNAVAF_myEst)
-# 
+#
 # cov_gatk <- parse_gatk_coverage(path_to_gatk_coverage = path_to_gatk_coverage,truth_set = subset(truth_set,variant_type %in% "SNV"),TCGA = TRUE)
-# 
+#
 # # leucegene
 # truth_set <- read_csv(file.path("/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/02-variant_loss_downsampling/02-variant_loss_downsampling_data","mut_for_paper_table.csv"))
 # truth_set <- truth_set %>% dplyr::rename(Feature = ensembl_transcript_id,
 #                                                 alt_initial = alt_initial_vardict) %>%
 #   separate(Locus, into=c("chrom","pos"),remove=FALSE,sep=":")
-# 
+#
 # path_to_gatk_coverage1 <- "/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/06-Streamline_downsampling/05-DepthOfCoverage/20M_100/20M_100"
 # cov_gatk <- parse_gatk_coverage(path_to_gatk_coverage = path_to_gatk_coverage1,truth_set = subset(truth_set,variant_type %in% "SNV"),TCGA = FALSE)
 
