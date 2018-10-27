b098dda42161800a2f670e12dd8d4d2f6da17607
Anna Quaglieri <quaglieri.a@wehi.edu.au>
2018-10-27 11:48:53 +1100

    corrected tot_depth_GATK assignment in parse_gatk_coverage.R

--- R/parse_gatk_coverage.R
+++ R/parse_gatk_coverage.R
@@ -20,11 +20,10 @@ cov_long_alt_depth <- cov %>% gather(base_alt_counts,alleles, contains("base_cou
 if(TCGA){
   names_samples <- unique(gsub("-",".",truth_set$SampleNameShort))
 }else{
-  names_samples <- unique(gsub("-",".",truth_set$SampleName))
+  names_samples <- unique(truth_set$SampleName)
 }
 
 create_match_names_tot <- data.frame(base_tot_counts = unique(cov_long_tot_depth$base_tot_counts))
-
 for(i in 1:nrow(create_match_names_tot)){
   mat <- stringr::str_extract(create_match_names_tot$base_tot_counts[i], names_samples) # extract sample name from every column name
   mat <- mat[!is.na(mat)]
@@ -41,10 +40,15 @@ for(i in 1:nrow(create_match_names_alt)){
 }
 
 # Match Samplename to alt and tot depth
-cov_long_alt_depth1 <- cov_long_alt_depth %>% dplyr::left_join(create_match_names_alt) %>% select(- base_alt_counts)
-cov_long_tot_depth1 <- cov_long_tot_depth %>% dplyr::left_join(create_match_names_tot) %>% select(- base_tot_counts)
+# I ran the depth of coverage for GATK on more samples that the ones I am greping for (aka the ones present in the truth set)
+# This means that some columns won't be present in my final set
+cov_long_alt_depth1 <- cov_long_alt_depth %>% dplyr::left_join(create_match_names_alt) %>%
+  dplyr::filter(!is.na(SampleName)) %>% dplyr::select(- base_alt_counts)
 
-cov_long <- cov_long_alt_depth1 %>% dplyr::full_join(cov_long_tot_depth1)
+cov_long_tot_depth1 <- cov_long_tot_depth %>% dplyr::left_join(create_match_names_tot) %>%
+  dplyr::filter(!is.na(SampleName)) %>% dplyr::select(- base_tot_counts)
+
+cov_long <- cov_long_alt_depth1 %>% dplyr::left_join(cov_long_tot_depth1)
 
 # Trick for TCGA names
 if(TCGA){
@@ -58,12 +62,11 @@ cov_long_infos1 <- cov_long_infos %>% separate(alleles,into = c("A","C","G","T",
 #head(cov_long_infos1[is.na(cov_long_infos1$A),c("SampleNameShort","Locus","A","C")])
 #cov_long_infos1[,c("SampleNameShort","Locus","A","C")]
 
-cov_long_infos1 <- cov_long_infos1 %>% dplyr::rename(tot_depth_GATK = tot_depth)
-
 # Total depth
-cov_long_infos1$tot_depth_GATK <- as.character(as.numeric(cov_long_infos1$tot_depth))
+cov_long_infos1 <- cov_long_infos1 %>% dplyr::rename(tot_depth_GATK = tot_depth)
+cov_long_infos1$tot_depth_GATK <- as.numeric(as.character(cov_long_infos1$tot_depth_GATK))
 
-# Alternative depth
+# Extract correct Alternative depth
 cov_long_infos1$alt_depth_GATK <- NA
 for(i in 1:nrow(cov_long_infos1)){
   allele_alt <- cov_long_infos1$alt_initial[i]
@@ -81,27 +84,27 @@ return(cov_long_infos1)
 # Test with TCGA
 
 # # TCGA
-# path_to_gatk_coverage <- "/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/06-Streamline_downsampling/05-DepthOfCoverage/TCGA_initial/TCGA_initial"
-# truth_set <- read_csv(file.path("/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/08-TCGA/02-prepare_for_sens_analysis_data/mut_infos_standardised.csv"))
-# truth_set$LongSampleName <- as.character(names40M$LongSampleName)[match(truth_set$SampleName,names40M$SampleName)]
-# length(unique(truth_set$LongSampleName)) # 139
-# length(unique(truth_set$SampleName)) # 139
-# truth_set <- truth_set %>% dplyr::rename(SampleNameShort = SampleName,
-#                                          SampleName = LongSampleName,
-#                                          Feature=ensembl_transcript_id) %>%
-#   select(SampleName,SampleNameShort,Locus,chrom,pos,end,ref,alt_initial,NCBI,SYMBOL,variant_type,Feature,PresentInRNA,
-#          NormalRefReads,NormalVarReads,NormalVAF,TumorRefReads,TumorVarReads,TumorVAF,RNARefReads,RNAVarReads,RNAVAF,RNAVAF_myEst)
-#
-# cov_gatk <- parse_gatk_coverage(path_to_gatk_coverage = path_to_gatk_coverage,truth_set = subset(truth_set,variant_type %in% "SNV"),TCGA = TRUE)
-#
-# # leucegene
-# truth_set <- read_csv(file.path("/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/02-variant_loss_downsampling/02-variant_loss_downsampling_data","mut_for_paper_table.csv"))
-# truth_set <- truth_set %>% dplyr::rename(Feature = ensembl_transcript_id,
-#                                                 alt_initial = alt_initial_vardict) %>%
-#   separate(Locus, into=c("chrom","pos"),remove=FALSE,sep=":")
-#
-# path_to_gatk_coverage1 <- "/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/06-Streamline_downsampling/05-DepthOfCoverage/20M_100/20M_100"
-# cov_gatk <- parse_gatk_coverage(path_to_gatk_coverage = path_to_gatk_coverage1,truth_set = subset(truth_set,variant_type %in% "SNV"),TCGA = FALSE)
+path_to_gatk_coverage <- "/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/06-Streamline_downsampling/05-DepthOfCoverage/TCGA_initial/TCGA_initial"
+truth_set <- read_csv(file.path("/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/08-TCGA/02-prepare_for_sens_analysis_data/mut_infos_standardised.csv"))
+truth_set$LongSampleName <- as.character(names40M$LongSampleName)[match(truth_set$SampleName,names40M$SampleName)]
+length(unique(truth_set$LongSampleName)) # 139
+length(unique(truth_set$SampleName)) # 139
+truth_set <- truth_set %>% dplyr::rename(SampleNameShort = SampleName,
+                                          SampleName = LongSampleName,
+                                          Feature=ensembl_transcript_id) %>%
+   select(SampleName,SampleNameShort,Locus,chrom,pos,end,ref,alt_initial,NCBI,SYMBOL,variant_type,Feature,PresentInRNA,
+          NormalRefReads,NormalVarReads,NormalVAF,TumorRefReads,TumorVarReads,TumorVAF,RNARefReads,RNAVarReads,RNAVAF,RNAVAF_myEst)
+
+ cov_gatk <- parse_gatk_coverage(path_to_gatk_coverage = path_to_gatk_coverage,truth_set = subset(truth_set,variant_type %in% "SNV"),TCGA = TRUE)
+
+ # leucegene
+ truth_set <- read_csv(file.path("/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/02-variant_loss_downsampling/02-variant_loss_downsampling_data","mut_for_paper_table.csv"))
+ truth_set <- truth_set %>% dplyr::rename(Feature = ensembl_transcript_id,
+                                                 alt_initial = alt_initial_vardict) %>%
+   separate(Locus, into=c("chrom","pos"),remove=FALSE,sep=":")
+
+ path_to_gatk_coverage1 <- "/stornext/Genomics/data/AML_RNA/quaglieri.a/GEO_Leucegene_data/scripts/06-Streamline_downsampling/05-DepthOfCoverage/20M_100/20M_100"
+ cov_gatk <- parse_gatk_coverage(path_to_gatk_coverage = path_to_gatk_coverage1,truth_set = subset(truth_set,variant_type %in% "SNV"),TCGA = FALSE)
 
 
 
