d702ee2e4952e84f0ba1d7b23560a6baace70561
Anna Quaglieri <anna.quaglieri16@gmail.com>
2019-01-22 17:45:45 +1100

    updated sens_compute and sensitivity_by_thresholds for use with INDELs

--- R/sens_compute.R
+++ R/sens_compute.R
@@ -1,13 +1,35 @@
 sens_compute <- function(variants,
-                         truth_set){
+                         truth_set,
+                         indel = FALSE){
 
-  # Sensitivity for SNVs using default filters + PON
-  sens_defaults = sum(variants$Called_defaults)/nrow(truth_set)
+  if(!indel){
 
-  # Sensitivity for SNVs using annotations + quality measures
-  sens_annot = sum(variants$Called_annot)/nrow(truth_set)
-  sens = c(sens_defaults,sens_annot)
-  names(sens) <- c("defaults","annotations")
+    # Sensitivity for SNVs using default filters + PON
+    sens_defaults = sum(variants$Called_defaults)/nrow(truth_set)
+
+    # Sensitivity for SNVs using annotations + quality measures
+    sens_annot = sum(variants$Called_annot)/nrow(truth_set)
+    sens = c(sens_defaults,sens_annot)
+    names(sens) <- c("defaults","annotations")
+
+  } else {
+
+    # Sensitivity for SNVs using default filters + PON
+    ncalled_annot <- length(unique(variants$key[variants$Called_annot == 1]))
+    ncalled_default <- length(unique(variants$key[variants$Called_defaults == 1]))
+    ncalled_annot05 <- length(unique(variants$key[variants$Called_annot05 == 1]))
+    ncalled_default05 <- length(unique(variants$key[variants$Called_defaults05 == 1]))
+
+    # Sensitivity for SNVs using annotations + quality measures
+    sens_annot = ncalled_annot/nrow(truth_set)
+    sens_annot05 = ncalled_annot05/nrow(truth_set)
+    sens_defaults = ncalled_default/nrow(truth_set)
+    sens_defaults05 = ncalled_default05/nrow(truth_set)
+
+    sens = c(sens_defaults,sens_annot,sens_defaults05,sens_annot05)
+    names(sens) <- c("defaults","annotations","defaults05","annotations05")
+
+  }
 
   return(sens)
 
--- R/sensitivity_by_threshold.R
+++ R/sensitivity_by_threshold.R
@@ -5,13 +5,13 @@
 
 
 sensitivity_by_thresholds <- function(variants,
-                         truth_set){
+                                      truth_set,indel = FALSE){
 
   # by different levels of depth
   depth_seq=seq(0,200,5)
   sens_by_depth <- sapply(depth_seq,function(d) {
-    sens_compute(variants=subset(variants,tot_depth >= d),
-                  truth_set=truth_set)}
+    sens_compute(variants =subset(variants,tot_depth >= d),
+                 truth_set = truth_set,indel = indel)}
   )
   colnames(sens_by_depth) <- depth_seq
 
@@ -19,13 +19,13 @@ sensitivity_by_thresholds <- function(variants,
   vaf_seq=seq(0,1,by = 0.01)
   sens_by_vaf <- sapply(vaf_seq,function(vaf) {
     sens_compute(variants=subset(variants,VAF >= vaf),
-                 truth_set=truth_set)}
+                 truth_set=truth_set,indel = indel)}
   )
   colnames(sens_by_vaf) <- vaf_seq
 
   # overall by the caller
   sens_overall <- sens_compute(variants=variants,
-                               truth_set=truth_set)
+                               truth_set=truth_set,indel = indel)
 
   # Output variants missed
   variants_missed_defaults <- variants[variants$Called_defaults != 1,]
